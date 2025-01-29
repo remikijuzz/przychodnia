349b1e7fcc4fc5aeb4386fbd46494c5a785d646a
Remigiusz Gawenda <remigiusz.gawenda@gmail.com>
2025-01-29 12:56:37 -0500

    Poprawiony Dyrektor - brak generowania pacjentow po sygnale rejestracji zamknietej

--- src/main.c
+++ src/main.c
@@ -20,19 +20,35 @@ pthread_mutex_t clinic_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* patient_generator(void* arg) {
     (void)arg;
-    for (int i = 0; i < 10; i++) { 
+    for (int hour = CLINIC_OPEN_HOUR; hour <= CLINIC_CLOSE_HOUR; hour++) {
         pthread_mutex_lock(&clinic_mutex);
-        if (clinic_closing) {
+        if (clinic_closing) {  // Jeśli rejestracja jest zamknięta, przerywamy generowanie pacjentów
             pthread_mutex_unlock(&clinic_mutex);
-            break;  // Nie dodajemy nowych pacjentów
+            printf("Rejestracja zamknięta - nowi pacjenci nie są przyjmowani.\n");
+            break;
         }
         pthread_mutex_unlock(&clinic_mutex);
 
-        Patient p = { rand() % 70 + 1, rand() % 2, rand() % NUM_DOCTORS, rand() % 90 };
-        add_patient_to_queue(p);
-}
+        printf("Godzina %d:00 - Pacjenci przychodzą.\n", hour);
+        
+        for (int i = 0; i < 10; i++) { // 10 pacjentów na godzinę
+            pthread_mutex_lock(&clinic_mutex);
+            if (clinic_closing) {  // Sprawdzamy ponownie wewnątrz pętli
+                pthread_mutex_unlock(&clinic_mutex);
+                printf("Rejestracja zamknięta - reszta pacjentów nie jest przyjmowana.\n");
+                break;
+            }
+            pthread_mutex_unlock(&clinic_mutex);
+
+            Patient p = { rand() % 70 + 1, rand() % 2, rand() % NUM_DOCTORS, rand() % 90 };
+            add_patient_to_queue(p);
+        }
+
+        sleep(1);  // Symulacja godziny
+    }
     return NULL;
 }
+
 
 void* doctor_thread(void* arg);
 
